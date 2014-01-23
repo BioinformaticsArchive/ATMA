005cdd3cf005e2f02bed41e6f3289a3baf3b5f19
Robert Walecki <Robert148@me.com>
2014-01-23 19:06:04 +0100

    add more unionfinder tests

--- tests/UnionFinder_Test.py
+++ tests/UnionFinder_Test.py
@@ -11,40 +11,40 @@ class UnionFinder_Test:
 
         assert a.GroupIDs == None
 
-    #def test_UnionFinder1(self):
+    def test_UnionFinder1(self):
 
-        #L1=[    [1,2],\
-                #[2,3],\
-                #[5,6],\
-                #[6,7],\
-                #[7,8]  ]
+        L1=[    [1,2],\
+                [2,3],\
+                [5,6],\
+                [6,7],\
+                [7,8]  ]
 
-        #a=ATMA.GapClosing.UnionFinder(L1)
-        #a.calcGroupIDs()
+        a=ATMA.GapClosing.DataStructures.UnionFinder(L1)
+        a.calcGroupIDs()
 
-        #assert a.GroupIDs[7] == 5
-        #assert a.GroupIDs[1] == 1
+        assert a.GroupIDs[7] == 5
+        assert a.GroupIDs[1] == 1
 
-    #def test_UnionFinder2(self):
+    def test_UnionFinder2(self):
 
-        #L2=[    [1,1],\
-                #[2,2],\
-                #[2,1],\
-                #[3,8],\
-                #[5,6]  ]
+        L2=[    [1,1],\
+                [2,2],\
+                [2,1],\
+                [3,8],\
+                [5,6]  ]
 
-        #a=ATMA.GapClosing.UnionFinder(L2)
-        #a.calcGroupIDs()
+        a=ATMA.GapClosing.DataStructures.UnionFinder(L2)
+        a.calcGroupIDs()
 
-        #assert a.GroupIDs[1],2
-        #assert a.GroupIDs[8],3
-        #assert a.GroupIDs[6],5
+        assert a.GroupIDs[1],2
+        assert a.GroupIDs[8],3
+        assert a.GroupIDs[6],5
 
-    #def test_UnionFinder3(self):
+    def test_UnionFinder3(self):
 
-        #L3=[[1,1]]
+        L3=[[1,1]]
 
-        #a=ATMA.GapClosing.UnionFinder(L3)
-        #a.calcGroupIDs()
+        a=ATMA.GapClosing.DataStructures.UnionFinder(L3)
+        a.calcGroupIDs()
 
-        #assert a.GroupIDs[1],1
+        assert a.GroupIDs[1],1
