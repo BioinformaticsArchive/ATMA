60962dd78fa4661e26ff0babb2242e59e1f5e85d
Robert Walecki <Robert148@me.com>
2014-01-23 18:55:36 +0100

    add tests for connector and unionfinder

---
+++ tests/Connector_test.py
@@ -0,0 +1,90 @@
+from nose.tools import *
+import ATMA
+import numpy
+
+class Connector_Test:
+    def setup(self):
+        TList=[]
+        for i in range(7):
+            newToken=ATMA.GapClosing.Structures.Token()
+            TList.append( newToken )
+
+        for t in range(len(TList)):
+            TList[t].ID=t
+
+        EList=[]
+        for i in range(7):
+            newEndPoint=ATMA.GapClosing.Structures.EndPoint()
+            newEndPoint.Thickness=i
+            newEndPoint.Token=TList[i]
+            EList.append( newEndPoint )
+
+        EList[0].Position, EList[0].Orientation, = [0,0,0], [0,0, 1]
+        EList[1].Position, EList[1].Orientation, = [0,0,3], [0,0,-1]
+        EList[2].Position, EList[2].Orientation, = [0,1,1], [0,0, 1]
+        EList[3].Position, EList[3].Orientation, = [0,1,4], [0,0,-1]
+        EList[4].Position, EList[4].Orientation, = [0,3,2], [0,0,-1]
+        EList[5].Position, EList[5].Orientation, = [0,4,4], [0,0, 1]
+        EList[6].Position, EList[6].Orientation, = [0,6,0], [0,0, 1]
+
+        self.t=ATMA.GapClosing.Connector(EList)
+
+    def test_empty(self):
+        EList=[]
+        t=ATMA.GapClosing.Connector(EList)
+        t.run()
+        assert t.EList_Matching==[]
+
+    def test_calcCostMatrix(self):
+        # check if costmarix does not exist
+        assert self.t.costMatrix==None
+
+        # run algorithm
+        self.t._calcCostMatrix()
+
+        # check if costmatrix exist
+        assert self.t.costMatrix!=None
+        #assert self.t.costMatrix.shape==(4,3)
+
+    def test_solveASP(self):
+
+         # run algorithm
+        self.t._calcCostMatrix()
+        self.t._solveASP()
+
+        # check if matching list has proper length and elements are different
+        assert len(self.t.EList_Matching)!=0
+        for e1,e2 in self.t.EList_Matching:
+            assert e1!=e2
+
+    def test_basic(self):
+
+        self.t.run()
+        res = self.t.EList_Matching
+
+        assert res[0][0].Token.ID == 0 and res[0][1].Token.ID == 1
+        assert res[1][0].Token.ID == 2 and res[1][1].Token.ID == 3
+        assert res[2][0].Token.ID == 6 and res[2][1].Token.ID == 4
+
+    def test_large(self):
+        EList=[]
+        for i in range(100):
+            x1,x2,x3 = numpy.random.rand(1,3)[0]
+            o1,o2,o3 = 2*numpy.random.rand(1,3)[0]-1
+
+            newEndPoint = ATMA.GapClosing.Structures.EndPoint()
+            newEndPoint.Position = [x1,x2,x3]
+            newEndPoint.Orientation = [o1,o2,o3]
+
+            EList.append( newEndPoint )
+
+        t=ATMA.GapClosing.Connector(EList)
+        #t.maxDist=0.5
+        t.run()
+        A=len(t.EList_Matching)
+
+        t.maxDist=0.2
+        t.run()
+        B=len(t.EList_Matching)
+
+        assert A>B
---
+++ tests/UnionFinder_Test.py
@@ -0,0 +1,50 @@
+from nose.tools import *
+import ATMA
+class UnionFinder_Test:
+
+    def test_UnionFinder_NoData(self):
+
+        L0=None
+
+        a=ATMA.GapClosing.DataStructures.UnionFinder(L0)
+        a.calcGroupIDs()
+
+        assert a.GroupIDs == None
+
+    #def test_UnionFinder1(self):
+
+        #L1=[    [1,2],\
+                #[2,3],\
+                #[5,6],\
+                #[6,7],\
+                #[7,8]  ]
+
+        #a=ATMA.GapClosing.UnionFinder(L1)
+        #a.calcGroupIDs()
+
+        #assert a.GroupIDs[7] == 5
+        #assert a.GroupIDs[1] == 1
+
+    #def test_UnionFinder2(self):
+
+        #L2=[    [1,1],\
+                #[2,2],\
+                #[2,1],\
+                #[3,8],\
+                #[5,6]  ]
+
+        #a=ATMA.GapClosing.UnionFinder(L2)
+        #a.calcGroupIDs()
+
+        #assert a.GroupIDs[1],2
+        #assert a.GroupIDs[8],3
+        #assert a.GroupIDs[6],5
+
+    #def test_UnionFinder3(self):
+
+        #L3=[[1,1]]
+
+        #a=ATMA.GapClosing.UnionFinder(L3)
+        #a.calcGroupIDs()
+
+        #assert a.GroupIDs[1],1
