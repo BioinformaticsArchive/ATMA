88415bc256e5679e5c65fd37b50ae152345a2991
Robert Walecki <Robert148@me.com>
2014-01-30 19:15:54 +0100

    first working GUI version

--- GUI/run.py
+++ GUI/run.py
@@ -1,4 +1,6 @@
 from qt_embedding import *
+import h5py
+import numpy #can be removed
 import ATMA
 
 
@@ -16,14 +18,17 @@ class ATMA_GUI(QtGui.QWidget):
 
         #Buttons
         self._button(0, 0, 1, 1, self._openFile,    "Open File"         )
-        self._button(1, 0, 1, 1, self._clear,       "Clear"             )
-        self._button(2, 0, 1, 1, self._demo,        "Demo"              )
-        self._button(3, 0, 1, 1, self._viewResults, "View Results"      )
-
+        self._button(2, 0, 1, 1, self._clear,       "Clear"             )
+        self._button(3, 0, 1, 1, self._demo,        "Demo"              )
+        self._button(4, 0, 1, 1, self._viewResults, "View Results"      )
+        self._button(5, 0, 1, 1, self._viewPrediction, "View Prediction")
+        self._button(17,0, 1, 1, self._run, "Run"      )
+        self._label(6, 0, 1, 1, self._set_sigmaSmooth, "Smoothing" , "0.7")
+        self._label(8, 0, 1, 1, self._set_thresMembra, "Thrashold" , "0.7")
 
         #Maya Widget
         self.M = MayaviQWidget(self)
-        self.grid.addWidget(self.M, 0, 3,18,1)
+        self.grid.addWidget(self.M, 0, 1,18,2)
 
         #Layout
         self.setGeometry(2300, 0, 1250, 650)
@@ -33,6 +38,19 @@ class ATMA_GUI(QtGui.QWidget):
 
 
     #Widgets
+    def _label(self,x,y,w,t,func, title, startValue):
+
+        q = QtGui.QLineEdit(self)
+        q.textChanged[str].connect(func)
+        q.setPlaceholderText(startValue)
+
+
+        l = QtGui.QLabel(self)
+        l.setText(title)
+
+        self.grid.addWidget(l,x,y,w,t)
+        self.grid.addWidget(q,x+1,y,w,t)
+        func(float(startValue))
 
 
     def _button(self,x,y,w,t,func,title):
@@ -40,30 +58,37 @@ class ATMA_GUI(QtGui.QWidget):
         b.clicked.connect(func)
         self.grid.addWidget(b,x,y,w,t)
 
-
     #Functions
-
+    def _set_sigmaSmooth(self, text):self.sigmaSmooth=float(text)
+    def _set_thresMembra(self, text):self.thresMembra=float(text)
+    def _viewResults(self):ATMA.GUI.DataVisualizer.segmentation( self.res )
+    def _viewPrediction(self):ATMA.GUI.DataVisualizer.rawSlider( self.pre )
 
     def _openFile(self):
         self.fname = QtGui.QFileDialog.getOpenFileName(self, 'Open file','/home/')
-        print self.fname
-
-
-    def _viewResults(self):
-        self.fname = QtGui.QFileDialog.getOpenFileName(self, 'Open file','/home/')
-
+        self.predictionData = h5py.File(self.fname)["volume/data"]
 
     def _clear(self):
         self.M.visualization.clear()
 
-
     def _demo(self):
         self.M.visualization.update_plot()
 
+    def _run(self):
 
-    def run(self):
-        a=ATMA.Segmentation.BioData.Nerve(self.fname)
+        A=ATMA.CLT()
+        A.sigmaSmooth = self.sigmaSmooth
+        A.thresMembra = self.thresMembra
 
+        #for debugging, remove in final version!
+        A.predictionData = h5py.File("./tests/data/Volume001.h5")["volume/data"]
+        A.sizeFilter = [20,1000]
+
+        A.run()
+        print "--Done--"
+
+        self.res = A.res
+        self.pre = A.predictionData[:,:,:,0]
 
 
 def main():
