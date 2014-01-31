11e1deb7835d4181404bf3f6c6fc12057025cddf
Robert Walecki <Robert148@me.com>
2014-01-31 18:22:13 +0100

    add a lot of widgets (skeletons)

--- GUI/run.py
+++ GUI/run.py
@@ -14,33 +14,76 @@ class ATMA_GUI(QtGui.QWidget):
     def initUI(self):
 
         self.grid = QtGui.QGridLayout()
-        self.grid.setSpacing(18)
+        #self.grid.setSpacing(18)
 
         #Buttons
-        self._button(0, 0, 1, 1, self._openFile,    "Open File"         )
-        self._button(2, 0, 1, 1, self._clear,       "Clear"             )
-        self._button(3, 0, 1, 1, self._demo,        "Demo"              )
-        self._button(4, 0, 1, 1, self._viewResults, "View Results"      )
-        self._button(5, 0, 1, 1, self._viewPrediction, "View Prediction")
-        self._button(17,0, 1, 1, self._run, "Run"      )
-        self._label(6, 0, 1, 1, self._set_sigmaSmooth, "Smoothing" , "0.7")
-        self._label(8, 0, 1, 1, self._set_thresMembra, "Thrashold" , "0.7")
+        self._buttonLabel(0, 0, self.test, "Open File" , "Path [string]")
+        self._buttonLabel(1, 0, self.test, "Save File" , "Path [string]")
+        self._label(2, 0, self._set_sigmaSmooth, "Range" , "[x0,x1,y0,y1,z0,z1]")
+        self._button(3, 0, self._viewPrediction, "View Prediction")
+
+        self._text(5,0, "Segmentation")
+        self._label(6, 0, self._set_sigmaSmooth, "Smoothing" , "Sigma [float]")
+        self._label(7, 0, self._set_thresMembra, "Thrasholding" , "Level [float]")
+        self._label(8, 0, self._set_thresMembra, "Closing" , "Pixel [intager]")
+        self._button2(9, 0, self.test, "Preview",self.test, "Parameter Estimation")
+
+        self._text(11,0, "Gap Closing")
+        #METHOD raider here !
+        self._label(12, 0, self._set_sigmaSmooth, "Max. Distance" , "Pixel [intager]")
+        self._label(13, 0, self._set_sigmaSmooth, "Min. Distance" , "Pixel [intager]")
+        self._button2(14, 0, self.test, "Preview",self.test, "Train Classifier")
+
+
+        self._text(16,0, "Gap Closing")
+        self._button3(17, 0, self.test, "Preview",self.test, "Training",self.test, "Done")
+        self._button(21, 0, self._run, "Run pipeline on subset"      )
+        self._button(22, 0, self._run, "Run pipeline on full dataset"      )
+        self._button(23, 0, self._viewResults, "View Results" )
 
         #Maya Widget
         self.M = MayaviQWidget(self)
-        self.grid.addWidget(self.M, 0, 1,18,2)
+        self.grid.addWidget(self.M, 0, 3,24,1)
 
         #Layout
-        self.setGeometry(2300, 0, 1250, 650)
+        self.setGeometry(2300, 0, 1250, 850)
         self.setLayout(self.grid)
         self.setWindowTitle('ATMA')
         self.show()
 
 
     #Widgets
-    def _label(self,x,y,w,t,func, title, startValue):
+    def _button(self,x,y,func,title):
+        b = QtGui.QPushButton(title,self)
+        b.clicked.connect(func)
+        self.grid.addWidget(b,x,y,1,3)
+
+    def _button2(self,x,y,func,title,func2,title2):
+        b = QtGui.QPushButton(title,self)
+        b.clicked.connect(func)
+        self.grid.addWidget(b,x,y,1,1)
+
+        b2 = QtGui.QPushButton(title2,self)
+        b2.clicked.connect(func2)
+        self.grid.addWidget(b2,x,y+1,1,2)
+
+    def _button3(self,x,y,func,title,func2,title2,func3,title3):
+        b = QtGui.QPushButton(title,self)
+        b.clicked.connect(func)
+        self.grid.addWidget(b,x,y,1,1)
+
+        b2 = QtGui.QPushButton(title2,self)
+        b2.clicked.connect(func2)
+        self.grid.addWidget(b2,x,y+1)
+
+        b3 = QtGui.QPushButton(title3,self)
+        b3.clicked.connect(func3)
+        self.grid.addWidget(b3,x,y+2)
+
+    def _label(self,x,y,func, title, startValue):
 
         q = QtGui.QLineEdit(self)
+        q.setMaximumWidth(200)
         q.textChanged[str].connect(func)
         q.setPlaceholderText(startValue)
 
@@ -48,21 +91,47 @@ class ATMA_GUI(QtGui.QWidget):
         l = QtGui.QLabel(self)
         l.setText(title)
 
-        self.grid.addWidget(l,x,y,w,t)
-        self.grid.addWidget(q,x+1,y,w,t)
-        func(float(startValue))
+        self.grid.addWidget(l,x,y)
+        self.grid.addWidget(q,x,y+1,1,2)
+
+        #if startValue has form of inatager or float, convert it
+        try:
+            startValue=float(startValue)
+        except ValueError:
+            pass
+
+        func(startValue)
+
+    def _text(self,x,y,title):
+
+        l = QtGui.QLabel(self)
+        l.setText("---"+title+"---")
+        self.grid.addWidget(l,x,y,1,3)
 
 
-    def _button(self,x,y,w,t,func,title):
+
+    def _buttonLabel(self,x,y,func,title,startValue):
         b = QtGui.QPushButton(title,self)
         b.clicked.connect(func)
-        self.grid.addWidget(b,x,y,w,t)
+        self.grid.addWidget(b,x,y,1,1)
+        q = QtGui.QLineEdit(self)
+        q.setMaximumWidth(200)
+        q.textChanged[str].connect(func)
+        q.setPlaceholderText(startValue)
+        self.grid.addWidget(q,x,y+1,1,2)
+
+
+
+
+
 
     #Functions
-    def _set_sigmaSmooth(self, text):self.sigmaSmooth=float(text)
-    def _set_thresMembra(self, text):self.thresMembra=float(text)
+    def _set_sigmaSmooth(self, text):self.sigmaSmooth=text
+    def _set_thresMembra(self, text):self.thresMembra=text
     def _viewResults(self):ATMA.GUI.DataVisualizer.segmentation( self.res )
     def _viewPrediction(self):ATMA.GUI.DataVisualizer.rawSlider( self.pre )
+
+    def test(self):pass
 
     def _openFile(self):
         self.fname = QtGui.QFileDialog.getOpenFileName(self, 'Open file','/home/')
@@ -92,6 +161,7 @@ class ATMA_GUI(QtGui.QWidget):
 
 
 def main():
+
     app = QtGui.QApplication.instance()
     ex = ATMA_GUI()
     app.exec_()
