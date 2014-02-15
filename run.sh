cc8c099d13ddc88b86ee565afd23b7083a8bbf61
Robert Walecki <Robert148@me.com>
2014-02-15 20:14:41 +0100

    less stuff gets imported

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 import PyQt4
-import GUI.main
+from src.main import ATMA_GUI
 
 def main():
 
     app = PyQt4.QtGui.QApplication.instance()
-    ex = GUI.main.ATMA_GUI()
+    ex = ATMA_GUI()
     app.exec_()
 
 if __name__ == '__main__':
