f1c7ca29a74d2daaa7ec4ab86ed60c07c95f8326
Robert Walecki <Robert148@me.com>
2014-02-19 18:01:44 +0100

    add blockprocess

--- ATMA/BlockProcess.py
+++ ATMA/BlockProcess.py
@@ -1,4 +1,21 @@
 import numpy
 import vigra
 import time
+import h5py
+
+from multiprocessing import Process, Lock
+import multiprocessing
+
+
+class BlockProcess():
+
+    data_in = None
+    data_out = None
+    Process = None
+
+    def __init__(self):pass
+
+    def run(self):
+        print numpy.mean(self.data_in)
+        print numpy.mean(self.Process(self.data_in))
 
--- tests/test_BlockProcess.py
+++ tests/test_BlockProcess.py
@@ -1,15 +1,31 @@
 import unittest
-import ATMA.BlockProcess
+from ATMA.BlockProcess import BlockProcess
 import numpy
+import h5py
 
-data_0=numpy.zeros((0,0,0))
-data_1=numpy.zeros((100,100,100))
-data_2=numpy.zeros((100,100,100))
-data_3=numpy.zeros((10,1,3))
-data_4=numpy.zeros((107,101,103))
+data_1 = numpy.random.random_integers(99,size=(0,0,0))
+data_2 = numpy.random.random_integers(99,size=(100,100,100))
+data_3 = numpy.random.random_integers(99,size=(101,107,113))
+data_5 = numpy.random.random_integers(99,size=(100,100,0))
+data_4 = numpy.random.random_integers(99,size=(100,1,100))
+
+def func(data):
+    return 2*data
 
 class testcase:
-    pass
+
+    def test_1_real(self):
+
+        data_in  = h5py.File("./data/vagus001.h5")["volume/data"][:,:,:,0]
+        f_out = h5py.File("./tmp.h5", "w")
+        data_out = f_out.create_dataset("volume/data", (data_in.shape), dtype=float, compression="lzf")
+
+        B = BlockProcess()
+        B.data_in = data_in
+        B.data_out = data_out
+        B.Process = func
+        B.run()
+        
 
 if __name__ == "__main__":
     import nose
